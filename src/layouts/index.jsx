34c75fd17869136dbeb8cd9f353acac9bbcad8ec
Jarod Taylor <jarodrtaylor@gmail.com>
2018-02-26 11:42:38 -0600

    updated gatsby-link

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../lib/global-styles';
 import theme from '../lib/theme';
